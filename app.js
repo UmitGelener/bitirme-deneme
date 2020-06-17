fc9669ad3e8afa129dae666d223440ff92ab1361
UmitGelener <43863130+UmitGelener@users.noreply.github.com>
2020-06-17 15:35:23 +0300

    mesaj gönderildi

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    res.end('Hello Travis!\n')
+    res.end('Hello Travis!\n');
 
 }).listen(1337, '127.0.0.1');
 
